1c5677321c1065becd2a357f039a395025f98e5f
Evan Dudla <me@evandudla.com>
2012-03-26 15:37:50 -0400

    Initial import of project

---
+++ src/evNN/Silex/Less/LessServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * LESS Compiler Service Provider for Silex PHP microframework.
+ *
+ * This provider uses the 'cypresslab/less-elephant' Composer package.
+ */
+
+namespace evNN\Silex\Less;
+
+use Silex\Application
+use Silex\ServiceProviderInterface;
+
+class LessServiceProvier implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+    }
+}
